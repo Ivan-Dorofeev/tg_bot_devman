77fd6add3eb572f56b88a41fb64db947a8111f65
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-25 09:43:01 +0300

    Добавил отправку сообщения ботом

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import os
 from pprint import pprint
 
 import requests
+import telegram
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
+def send_mail(tg_token, tg_chat_id):
+    tg_bot = telegram.Bot(token=tg_token)
+    tg_bot.send_message(chat_id=tg_chat_id, text="Привет, дружище")
 
-    # url = 'https://dvmn.org/api/user_reviews/'
+
+def main(api_devman_token):
     url = 'https://dvmn.org/api/long_polling/'
-    api_devman_token = os.environ['DEVMAN_API_TOKEN']
     authorization_token = f'Token {api_devman_token}'
 
     headers = {
@@ -20,12 +22,24 @@ def main():
     while True:
         try:
             response = requests.get(url, headers=headers)
-            response.raise_for_status()
         except requests.exceptions.ReadTimeout:
+            timestamp_to_request = response.json()['timestamp_to_request']
+            uploads = {
+                'timestamp': timestamp_to_request
+            }
+            response = requests.get(url, headers=headers, params=uploads)
+        except ConnectionError:
             response = requests.get(url, headers=headers)
-            response.raise_for_status()
+
+        response.raise_for_status()
         print(response.text)
 
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    tg_chat_id = os.environ['TG_MY_CHAT_ID']
+    tg_token = os.environ['TG_BOT_TOKEN']
+    api_devman_token = os.environ['DEVMAN_API_TOKEN']
+
+    # main(api_devman_token)
+    send_mail(tg_token, tg_chat_id)
