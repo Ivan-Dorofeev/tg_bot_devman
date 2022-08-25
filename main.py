32fabca6a0c48e985e3665d13d87424877194ec8
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-25 11:12:06 +0300

    Собрал все функции в один бот

--- main.py
+++ main.py
@@ -6,33 +6,42 @@ import telegram
 from dotenv import load_dotenv
 
 
-def send_mail(tg_token, tg_chat_id):
+def send_mail(tg_token, tg_chat_id, msg):
     tg_bot = telegram.Bot(token=tg_token)
-    tg_bot.send_message(chat_id=tg_chat_id, text="Привет, дружище")
+    tg_bot.send_message(chat_id=tg_chat_id, text=msg)
 
 
-def main(api_devman_token):
+def main(api_devman_token, tg_token, tg_chat_id):
     url = 'https://dvmn.org/api/long_polling/'
     authorization_token = f'Token {api_devman_token}'
 
     headers = {
         'Authorization': authorization_token
     }
+    uploads = {}
 
     while True:
         try:
-            response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=headers, params=uploads)
+            server_answer = response.json()
+            pprint(server_answer)
+
+            lessons_cheked = server_answer['new_attempts']
+            for lesson in lessons_cheked:
+                lesson_cheked_status = "Надо доработать" if lesson['is_negative'] else "Работат принята"
+                lesson_cheked_url = lesson['lesson_url']
+
+                message = f'Статус: {lesson_cheked_status}\nУрок: {lesson_cheked_url}'
+                send_mail(tg_token, tg_chat_id, message)
+
         except requests.exceptions.ReadTimeout:
             timestamp_to_request = response.json()['timestamp_to_request']
             uploads = {
                 'timestamp': timestamp_to_request
             }
-            response = requests.get(url, headers=headers, params=uploads)
+            continue
         except ConnectionError:
-            response = requests.get(url, headers=headers)
-
-        response.raise_for_status()
-        print(response.text)
+            continue
 
 
 if __name__ == '__main__':
@@ -41,5 +50,4 @@ if __name__ == '__main__':
     tg_token = os.environ['TG_BOT_TOKEN']
     api_devman_token = os.environ['DEVMAN_API_TOKEN']
 
-    # main(api_devman_token)
-    send_mail(tg_token, tg_chat_id)
+    main(api_devman_token, tg_token, tg_chat_id)
