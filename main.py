4c97d2bdaa1749fa65535d3610739a42ba75c66e
Ivan Dorofeev <axxel123@rambler.ru>
2022-09-04 08:09:42 +0300

    Добавил логирование

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ import time
 import requests
 import telegram
 from dotenv import load_dotenv
+import logging
+
+logger = logging.getLogger("tg_bot_logger>")
 
 
 def main():
+    logging.basicConfig(filename='tg_bot_log.log', format="%(asctime)s - %(funcName)s(%(lineno)d): %(message)s")
+    logger.setLevel(logging.INFO)
+    logging.warning('Бот запущен')
+
     load_dotenv()
     tg_chat_id = os.environ['TG_MY_CHAT_ID']
     tg_token = os.environ['TG_BOT_TOKEN']
@@ -49,8 +56,11 @@ def main():
             uploads = {
                 'timestamp': datetime.datetime.now().timestamp()
             }
+            logging.info('Долгое ожидание ответа')
             continue
         except ConnectionError:
+            logging.warning('Ошибка соединения')
+            tg_bot.send_message(chat_id=tg_chat_id, text='Ошибка соединения')
             time.sleep(30)
             continue
 
