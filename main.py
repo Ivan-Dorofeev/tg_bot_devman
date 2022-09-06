1878a04eb0144f9b2f6a22aa8390f883dbfc4016
Ivan Dorofeev <axxel123@rambler.ru>
2022-09-06 13:16:29 +0300

    Поправил логер

--- main.py
+++ main.py
@@ -10,18 +10,30 @@ import logging
 logger = logging.getLogger("tg_bot_logger>")
 
 
-def main():
-    logging.basicConfig(filename='tg_bot_log.log', format="%(asctime)s - %(funcName)s(%(lineno)d): %(message)s")
-    logger.setLevel(logging.INFO)
-    logging.warning('Бот запущен')
+class TelegramLogsHandler(logging.Handler):
 
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def main():
     load_dotenv()
     tg_chat_id = os.environ['TG_MY_CHAT_ID']
     tg_token = os.environ['TG_BOT_TOKEN']
     api_devman_token = os.environ['DEVMAN_API_TOKEN']
 
     tg_bot = telegram.Bot(token=tg_token)
-    tg_bot.send_message(chat_id=tg_chat_id, text='Бот запущен')
+
+    logging.basicConfig(filename='tg_bot_log.log', format="%(asctime)s - %(funcName)s(%(lineno)d): %(message)s")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(tg_bot, tg_chat_id))
+    logger.warning('Бот запущен')
 
     url = 'https://dvmn.org/api/long_polling/'
     authorization_token = f'Token {api_devman_token}'
@@ -49,6 +61,7 @@ def main():
 
                     message = f'Статус: {lesson_cheked_status}\nУрок: {lesson_cheked_url}'
                     tg_bot.send_message(chat_id=tg_chat_id, text=message)
+                    logger.info('ЕСТЬ СООБЩЕНИЕ ОТ ЛОГЕРА')
                 uploads = {
                     'timestamp': datetime.datetime.now().timestamp()
                 }
@@ -60,12 +73,11 @@ def main():
             logging.info('Долгое ожидание ответа')
             continue
         except ConnectionError:
-            logging.warning('Ошибка соединения')
-            tg_bot.send_message(chat_id=tg_chat_id, text='Ошибка соединения')
+            logger.warning('Ошибка соединения')
             time.sleep(30)
             continue
         except Exception as exc:
-            tg_bot.send_message(chat_id=tg_chat_id, text=exc)
+            logger.warning(exc)
             continue
 
 
